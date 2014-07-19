654f91cfaaa79b4042f7ffc75fea5fc48742a656
Andrew <No emails>
2014-07-19 23:21:52 +0400

    clientservice improved

--- clientservice.h
+++ clientservice.h
@@ -77,12 +77,14 @@ class clientservice : public internlmsgsender<D>
     const char * waiting_for_answer;
     const char * sending_hello;
     const char * answer_received;
+    const char * starting_byte_received;
+    const char * message_size_received;
     const char * closing_conn;
     const char * recv_error;
     const char * header_read_error;
 
-    bool read_nbytes(const ssize_t bytes_to_read, char * bfr);
-    bool send_nbytes(const ssize_t bytes_to_send, char * bfr);
+    bool read_nbytes(char * const bfr, const ssize_t bytes_to_read);
+    bool send_nbytes(char * const bfr, const ssize_t bytes_to_send);
     short unsigned int read_header();
 
 public:
@@ -99,6 +101,8 @@ clientservice<D>::clientservice(int sock_, const modulemanager<D> * const mod_mg
                                                             waiting_for_answer("[clientservice] waiting for answer"),
                                                             sending_hello("[clientservice] sending hello"),
                                                             answer_received("[clientservice] answer received: "),
+                                                            starting_byte_received("[clientservice] starting byte received"),
+                                                            message_size_received("[clientservice] message size received: "),
                                                             closing_conn("[clientservice] closing connection"),
                                                             recv_error("[clientservice] cannot receive message"),
                                                             header_read_error("[clientservice] cannot read header")
@@ -119,12 +123,13 @@ void clientservice<D>::operator()()
         {
             case INITIAL:
             {
-                int res = 0;
-                this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0, std::string(sending_hello));
-                std::string hello("hello!\n");
+//                int res = 0;
+//                this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0, std::string(sending_hello));
+//                std::string hello("hello!\n");
 
-                res = send(sock, hello.c_str() , hello.length(), 0);
-                state = WAITING_FOR_INIT_ANSWER;
+//                res = send(sock, hello.c_str() , hello.length(), 0);
+//                state = WAITING_FOR_INIT_ANSWER;
+                state = POST_REQUEST;
             }
             break;
             case WAITING_FOR_INIT_ANSWER:
@@ -136,7 +141,7 @@ void clientservice<D>::operator()()
                     arr_wrap<char> bfr(msg_size);
                     memset(bfr.at(0), 0, bfr.get_size());
 
-                    if (read_nbytes(msg_size, bfr.at(0)))
+                    if (read_nbytes(bfr.at(0), msg_size))
                     {
                         this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0,
                                                std::move(std::string(answer_received) + std::string(bfr.at(0))));
@@ -156,13 +161,30 @@ void clientservice<D>::operator()()
             break;
             case POST_REQUEST:
             {
+                arr_wrap<char> bfr(4);
+                *bfr.at(0) = '#';
+                *bfr.at(1) = 0x0;
+                *bfr.at(2) = 0x1;
+                *bfr.at(3) = 0x31;
+                bool res = send_nbytes(bfr.at(0), bfr.get_size());
             }
             break;
             case WAITING_FOR_ANSWER:
             {
                 this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0, std::move(std::string(waiting_for_answer)));
                 memset(chBfr, 0, 2);
-                recv(sock, chBfr, 2, 0);
+                recv(sock, chBfr, 1, 0);
+
+                if (chBfr[0] == '#')
+                {
+                    this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0,
+                                        std::move(std::string(starting_byte_received)));
+                    recv(sock, chBfr, 2, 0);
+                    int size = *(int*)chBfr;
+                    this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0,
+                                           std::move(std::string(message_size_received) + std::to_string(size)));
+                    recv(sock, chBfr, size, 0);
+                }
                 this->send_internl_msg(INTNLMSG::RECV_DISPLAY, 0,
                                     std::move(std::string(answer_received) + std::string(chBfr)));
                 state = CLOSE;
@@ -187,14 +209,14 @@ short unsigned int clientservice<D>::read_header()
     char size[2];
     short unsigned int res;
 
-    read_nbytes(2, size);
+    read_nbytes(size, 2);
     res = *(short unsigned int *)size;
 
     return res;
 }
 
 template<class D>
-bool clientservice<D>::read_nbytes(const ssize_t bytes_to_read, char *bfr)
+bool clientservice<D>::read_nbytes(char * const bfr, const ssize_t bytes_to_read)
 {
     bool res = false;
     char *bfr_ptr = bfr;
@@ -223,7 +245,7 @@ bool clientservice<D>::read_nbytes(const ssize_t bytes_to_read, char *bfr)
 }
 
 template<class D>
-bool clientservice<D>::send_nbytes(const ssize_t bytes_to_send, char *bfr)
+bool clientservice<D>::send_nbytes(char * const bfr, const ssize_t bytes_to_send)
 {
     bool res = false;
     char *bfr_ptr = bfr;
